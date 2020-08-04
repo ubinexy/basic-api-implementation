4ec80c24932c84b24a080c2ac140bcb0ccde869a
ubinexy <ubinexy@gmail.com>
2020-08-04 23:19:11 +0800

    feat: modify getRsEventList for getting subList of RsEvent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -12,7 +13,10 @@ public class RsController {
   private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
 
   @GetMapping(path="/rs/list")
-  public String getRsEventList() {
+  public String getRsEventList(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+    if(start != null && end != null) {
+      return rsList.subList(start-1, end).toString();
+    }
     return rsList.toString();
   }
 
