5a94a01d954c8d87564f7f447e347da0348641b6
ubinexy <ubinexy@gmail.com>
2020-08-05 23:14:45 +0800

    add test: should return list of users in json

--- src/test/java/com/thoughtworks/rslist/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/UserControllerTest.java
@@ -1,7 +1,10 @@
 package com.thoughtworks.rslist;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.api.RsController;
+import com.thoughtworks.rslist.api.UserController;
 import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,7 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -18,6 +26,14 @@ public class UserControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @Autowired
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        userController.userList = new ArrayList<User>();
+    }
+
     @Test
     void should_regsiter_user() throws Exception {
         User user = new User("default", "male", 18, "default@email.com", "18888888888");
@@ -25,5 +41,23 @@ public class UserControllerTest {
         String jsonStr = objectMapper.writeValueAsString(user);
         mvc.perform(post("/user").content(jsonStr).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void should_return_userList_when_getUserList() throws Exception {
+        User user = new User("default", "male", 20, "default@email.com", "18888888888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = objectMapper.writeValueAsString(user);
+        mvc.perform(post("/user").content(jsonStr).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        mvc.perform(get("/users").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].user_name", is("default")))
+                .andExpect(jsonPath("$[0].user_gender", is("male")))
+                .andExpect(jsonPath("$[0].user_age", is(20)))
+                .andExpect(jsonPath("$[0].user_email", is("default@email.com")))
+                .andExpect(jsonPath("$[0].user_phone", is("18888888888")));
     }
 }
