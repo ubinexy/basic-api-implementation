77aaca2a9862b59d7d267e8bc6dc1b14c2983afb
ubinexy <ubinexy@gmail.com>
2020-08-07 01:32:54 +0800

    feat: event will patch only when userId is the same

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -7,9 +7,7 @@ import com.thoughtworks.rslist.repository.RsEventRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.awt.*;
@@ -39,5 +37,17 @@ public class RsController {
 
     rsEventRepository.save(eventDto);
     return ResponseEntity.created(null).build();
+  }
+
+  @PatchMapping("/rs/event/{id}")
+  ResponseEntity updateEvent(@PathVariable Integer id, @RequestBody RsEvent event) {
+      RsEventDto eventDto = rsEventRepository.findById(id).get();
+      if(eventDto.getUserDto().getId() != event.getUserId()) {
+        return ResponseEntity.badRequest().build();
+      }
+      if(event.getEventName() != null) eventDto.setEventName(event.getEventName());
+      if(event.getKeyword() != null) eventDto.setKeyword(event.getKeyword());
+      rsEventRepository.save(eventDto);
+      return ResponseEntity.ok().build();
   }
 }
