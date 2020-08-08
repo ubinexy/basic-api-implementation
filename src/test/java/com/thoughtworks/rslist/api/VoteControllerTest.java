ba6649ae6096cd6d2c3bd4ab8f6d0d052e6f39a9
ubinexy <ubinexy@gmail.com>
2020-08-08 16:20:35 +0800

    refactor test: should_vote_success..() should be independent of test order

--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.RsEventDto;
 import com.thoughtworks.rslist.dto.UserDto;
+import com.thoughtworks.rslist.dto.VoteDto;
 import com.thoughtworks.rslist.repository.RsEventRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.repository.VoteRepository;
 
 import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.BeforeEach;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +23,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -34,12 +41,15 @@ public class VoteControllerTest {
     private VoteRepository voteRepository;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
+    private UserDto defaultUser;
+    private RsEventDto eventDto;
+
     @BeforeEach
     private void setUp() {
         voteRepository.deleteAll();
         userRepository.deleteAll();
         rsEventRepository.deleteAll();
-        UserDto userDto = UserDto.builder()
+        defaultUser = UserDto.builder()
                 .username("default")
                 .gender("male")
                 .age(18)
@@ -47,33 +57,81 @@ public class VoteControllerTest {
                 .phone("18888888888")
                 .voteNum(10)
                 .build();
-        userRepository.save(userDto);
+        userRepository.save(defaultUser);
+
+        eventDto = RsEventDto.builder()
+                .eventName("热搜事件名")
+                .keyword("关键字")
+                .userDto(defaultUser)
+                .build();
+        rsEventRepository.save(eventDto);
+
+        VoteDto voteDto1 = VoteDto.builder()
+                .num(2)
+                .localDateTime(LocalDateTime.parse("2020-08-01T10:00:00", formatter))
+                .user(defaultUser)
+                .rsEvent(eventDto)
+                .build();
+
+        VoteDto voteDto2 = VoteDto.builder()
+                .num(4)
+                .localDateTime(LocalDateTime.parse("2020-08-02T10:00:00", formatter))
+                .user(defaultUser)
+                .rsEvent(eventDto)
+                .build();
+
+        voteRepository.save(voteDto1);
+        voteRepository.save(voteDto2);
     }
 
     @Test
     void should_vote_success_given_voteNum_less_then_number_of_tickets_left() throws Exception {
 
-        String firstEvent = "{\"eventName\":\"热搜事件名\",\"keyword\":\"关键字\",\"userId\":1}";
-        mockMvc.perform(post("/rs/event").content(firstEvent).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
-
-        UserDto user = userRepository.findById(1).get();
+        UserDto user = userRepository.findById(defaultUser.getId()).get();
         assertEquals(10, user.getVoteNum());
 
-        String firstVote = "{\"voteNum\":3,\"userId\":1,\"voteTime\":\""+ printTime() + "\"}";
-        mockMvc.perform(post("/rs/vote/1").content(firstVote).contentType(MediaType.APPLICATION_JSON))
+        String firstVote = "{\"voteNum\":3,\"userId\":"+defaultUser.getId() +",\"voteTime\":\""+ printTime() + "\"}";
+        mockMvc.perform(post("/rs/vote/"+ eventDto.getId()).content(firstVote).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
-        user = userRepository.findById(1).get();
+        user = userRepository.findById(defaultUser.getId()).get();
         assertEquals(7, user.getVoteNum());
 
-        String SecondVote = "{\"voteNum\":8,\"userId\":1,\"voteTime\":\""+ printTime() + "\"}";
-        mockMvc.perform(post("/rs/vote/1").content(SecondVote).contentType(MediaType.APPLICATION_JSON))
+        String SecondVote = "{\"voteNum\":8,\"userId\":"+defaultUser.getId() +",\"voteTime\":\""+ printTime() + "\"}";
+        mockMvc.perform(post("/rs/vote/"+ eventDto.getId()).content(SecondVote).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 
     private String printTime() {
         LocalDateTime time = LocalDateTime.now();
         return time.format(formatter);
+    }
+
+//    @Test
+    void should_return_votes_between_start_and_end_dateTime() throws Exception {
+        String startDateTime = "2020-08-01T09:00:00";
+        String endDateTime = "2020-08-01T11:00:00";
+
+        mockMvc.perform(get("/rs/vote?page=1")
+                .param("from", startDateTime)
+                .param("to",endDateTime)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].vote_dateTime", is("2020-08-01T10:00:00")))
+                .andExpect(jsonPath("$[0].vote_number", is(2)));
+
+        endDateTime = "2020-08-02T11:00:00";
+
+        mockMvc.perform(get("/rs/vote?page=1")
+                .param("from", startDateTime)
+                .param("to",endDateTime)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].vote_dateTime", is("2020-08-01T10:00:00")))
+                .andExpect(jsonPath("$[0].vote_number", is(2)))
+                .andExpect(jsonPath("$[1].vote_dateTime", is("2020-08-01T10:00:00")))
+                .andExpect(jsonPath("$[1].vote_number", is(4)));
     }
 }
